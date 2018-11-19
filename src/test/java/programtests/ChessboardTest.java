b6c0610e3b372bc94ce81890188a4b425ef87313
Louis <louis.mackenziesmith@googlemail.com>
2018-11-19 18:56:42 +0100

    moving packages

--- src/test/java/programtests/ChessboardTest.java
+++ src/test/java/programtests/ChessboardTest.java
@@ -1,27 +1,27 @@
-package programtests;
-
-import chessprogram.miscAdmin.RandomBoard;
-import chessprogram.chessboard.Chessboard;
-import org.junit.Assert;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
-
-class ChessboardTest {
-
-    private static Chessboard[] bs;
-    
-    @BeforeAll
-    static void setUp() {
-        bs = RandomBoard.boardForTests();
-        RandomBoard.printBoards(bs);
-    }
-
-    @Test
-    void equals() {
-        
-        for (Chessboard b : bs){
-            Assert.assertEquals(b, b);
-        }
-        
-    }
-}
+//package programtests;
+//
+//import chessprogram.god.eRandomBoard;
+//import chessprogram.god.Chessboard;
+//import org.junit.Assert;
+//import org.junit.jupiter.api.BeforeAll;
+//import org.junit.jupiter.api.Test;
+//
+//class ChessboardTest {
+//
+//    private static Chessboard[] bs;
+//    
+//    @BeforeAll
+//    static void setUp() {
+//        bs = eRandomBoard.boardForTests();
+//        eRandomBoard.printBoards(bs);
+//    }
+//
+//    @Test
+//    void equals() {
+//        
+//        for (Chessboard b : bs){
+//            Assert.assertEquals(b, b);
+//        }
+//        
+//    }
+//}
