e6956d9506fea1eb6d620b5acfa94fb8bc5b267e
Louis <louis.mackenziesmith@googlemail.com>
2018-12-20 18:39:35 +0100

    move patterns out of their methods, deleted an enum object

--- src/test/java/com/github/louism33/chesscore/PerftTest.java
+++ src/test/java/com/github/louism33/chesscore/PerftTest.java
@@ -35,17 +35,17 @@ class PerftTest {
                 3195901860L);
 
 
-//        Assert.assertEquals(
-//                Perft.perftTest(8, new Chessboard(), 84998978956L),
-//                84998978956L);
+        Assert.assertEquals(
+                Perft.perftTest(8, new Chessboard(), 84998978956L),
+                84998978956L);
 
 
-//        Assert.assertEquals(
-//                Perft.runPerftTestWithBoardLong(9, new Chessboard(), 2439530234167L),
-//                2439530234167L);
+        Assert.assertEquals(
+                Perft.perftTest(9, new Chessboard(), 2439530234167L),
+                2439530234167L);
 //
 //        Assert.assertEquals(
-//                Perft.runPerftTestWithBoardLong(10, new Chessboard(), 69352859712417L),
+//                Perft.perftTest(10, new Chessboard(), 69352859712417L),
 //                69352859712417L);
     }
 
